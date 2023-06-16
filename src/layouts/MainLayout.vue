430eb07fab765ce4a2411f10a8526f19df3bb918
Gregory Ross <gregross.dev@gmail.com>
2023-06-16 12:40:34 -0400

    init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Header from "./components/Header.vue"
+import Footer from "./components/Footer.vue"
+</script>
+
+<template>
+  <div class="layout-wrapper">
+    <Header/>
+    <main>
+      <slot/>
+    </main>
+    <Footer/>
+  </div>
+</template>
+
---
+++ src/layouts/components/Footer.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <footer>
+    <RouterLink to="/">
+      Home
+    </RouterLink>
+  </footer>
+</template>
+
+<style>
+footer {
+  margin-top: auto;
+}
+</style>
