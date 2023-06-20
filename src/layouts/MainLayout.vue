df0d964e35cb8c6d5c535f3a6839d52e9c664b99
Gregory Ross <gregross.dev@gmail.com>
2023-06-20 12:18:26 -0400

    naming conventions

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <script setup>
-import Header from "./components/Header.vue"
-import Footer from "./components/Footer.vue"
+import Header from "./components/MainHeader.vue"
+import Footer from "./components/MainFooter.vue"
 </script>
 
 <template>
