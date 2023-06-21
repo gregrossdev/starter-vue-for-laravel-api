0faecd3434c2c72c7030c4d4c63033b0dd38892f
Gregory Ross <gregross.dev@gmail.com>
2023-06-20 22:12:04 -0400

    transitions

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <script setup>
-import { RouterView } from 'vue-router'
-import MainLayout from "@/layouts/MainLayout.vue";
-</script>
+import MainLayout from "@/layouts/MainLayout.vue";</script>
 
 <template>
   <MainLayout>
-    <RouterView />
+    <RouterView class="flex-1" v-slot="{ Component }">
+      <Transition name="page">
+        <component :is="Component" />
+      </Transition>
+    </RouterView>
   </MainLayout>
 </template>
+
+<style>
+.page-enter-active {
+  transition: 600ms ease all;
+}
+
+.page-enter-from {
+  opacity: 0;
+}
+</style>
